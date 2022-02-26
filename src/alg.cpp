beea47c1e25b1bdab90c62a72b2460b7e39e995e
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-02-26 22:40:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,17 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int i = 0, j = size - 1, k = 0; // i - левая граница массива, j - правая
+int mid = i + (j - i) / 2;
+for (int i = 0; i <= mid; i++) {
+  if (arr[i] == value) {
+    k+=1;        
+  }
+}
+for (int i = size; i > mid; i--) {
+  if (arr[i] == value) {
+    k += 1;    
+  }
+}
+return k;
 }
