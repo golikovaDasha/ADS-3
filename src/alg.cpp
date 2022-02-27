bae446e672d52a3d23ff258df97c4d5540a2390b
Golikova Dasha <99117131+golikovaDasha@users.noreply.github.com>
2022-02-27 18:58:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,12 @@ int i = 0, j = size - 1, k = 0; // i - левая граница массива,
 int mid = i + (j - i) / 2;
 for (int i = 0; i <= mid; i++) {
   if (arr[i] == value) {
-    k+=1;        
+   k+=1;        
   }
 }
 for (int i = size; i > mid; i--) {
   if (arr[i] == value) {
-    k += 1;    
+   k += 1;    
   }
 }
 return k;
